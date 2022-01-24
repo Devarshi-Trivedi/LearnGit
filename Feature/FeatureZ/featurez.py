15ca037f9fc9fd759b86c037e04d4c04d772d708
JPonda <pondajay637@gmail.com>
2022-01-24 22:21:17 +0530

    E

--- Feature/FeatureZ/featurez.py
+++ Feature/FeatureZ/featurez.py
@@ -1,1 +1,21 @@
 # list operation
+
+def giveInstOfError(stment):
+    print(stment)
+    exit()
+
+def inpvalidater(lst=None, *arg ):
+    
+    if lst == None or not isinstance(lst, list):
+        giveInstOfError('inpvalidater needs first argument as list')
+
+    if len(arg) == 0:
+        giveInstOfError('inpvalidater needs second argument as operation instruction')
+        
+    if isinstance(arg[0], str):
+        giveInstOfError('inpvalidater needs second argument as operation instruction in string type')
+
+    
+    
+
+    
