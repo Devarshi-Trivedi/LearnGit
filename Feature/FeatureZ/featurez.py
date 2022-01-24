0042d416451582dc915647743af814c0f4e28200
JPonda <pondajay637@gmail.com>
2022-01-25 01:49:42 +0530

    L

--- Feature/FeatureZ/featurez.py
+++ Feature/FeatureZ/featurez.py
@@ -23,7 +23,7 @@ def listop(lst=None, *arg ):
     if arg[0] == 'push':
         if len(arg) < 2:
             giveInstOfError('no element for push operation')
-        lst.push(arg[1])
+        lst.append(arg[1])
         return {
             'index': len(lst) - 1,
             'val': arg[1],
@@ -40,7 +40,40 @@ def listop(lst=None, *arg ):
             'list': lst,
         }
 
+    if arg[0] == 'pop':
+        if lst != []:
+            giveInstOfError('pop operation can not be perform on empty list')
+        return {
+            'index': len(lst) - 1,
+            'val': lst.pop(),
+            'list': lst
+        }
 
+    if arg[0] == 'unshift':
+        if lst != []:
+            giveInstOfError('unshift operation can not be perform on empty list')
+        return {
+            'index': 0,
+            'val': lst.pop(0),
+            'list': lst
+        }
+
+    if arg[0] == 'insert':
+        if len(arg) < 3 or arg[2] == None:
+            giveInstOfError('not enough arguments for insert operation')
+        if not isinstance(arg[1], int):
+            giveInstOfError('index should be in integer type')
+        if 0 > arg[1] > len(lst):
+            giveInstOfError('your given index is out of bound')
+
+        lst.insert(arg[1], arg[2])
+        return {
+            'index': arg[1],
+            'val': arg[2],
+            'list': lst
+        }
+
+    
     
     
 
